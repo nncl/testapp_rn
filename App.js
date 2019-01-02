93f3510a7fd6ee08e5d992d08d1780afa946f3b1
caue <nncl@live.com>
2019-01-02 18:56:40 -0200

    Initial commit

---
+++ App.js
@@ -0,0 +1,72 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
+
+import React, {Component} from 'react';
+import {Platform, StyleSheet, Text, View, ActivityIndicator} from 'react-native';
+
+const instructions = Platform.select({
+    ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
+    android:
+        'Double tap R on your keyboard to reload,\n' +
+        'Shake or press menu button for dev menu',
+});
+
+type Props = {};
+export default class App extends Component<Props> {
+
+    constructor() {
+        super();
+        this.state = {loading: false};
+    }
+
+    componentDidMount(): void {
+        this.load();
+    }
+
+    load(): void {
+        this.setState({loading: true});
+        setTimeout(() => {
+            this.setState({loading: false})
+        }, 3000)
+    }
+
+    render() {
+
+        const loading = this.state.loading ? <ActivityIndicator size="large" color="#0000ff"/> : null,
+            content = !this.state.loading ? <Text style={styles.welcome}>Welcome to React Native!</Text> : null;
+
+        return (
+            <View style={styles.container}>
+                {content}
+                {/*<Text style={styles.instructions}>To get started, edit App.js</Text>*/}
+                {/*<Text style={styles.instructions}>{instructions}</Text>*/}
+
+                {loading}
+            </View>
+        );
+    }
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#F5FCFF',
+    },
+    welcome: {
+        fontSize: 20,
+        textAlign: 'center',
+        margin: 10,
+    },
+    instructions: {
+        textAlign: 'center',
+        color: '#333333',
+        marginBottom: 5,
+    },
+});
